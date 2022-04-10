5a6b1d27969999d34235bd33c57ba5342a03a6b2
Andrew Truex <andrewtruex@gmail.com>
2022-04-10 06:02:25 -0400

    config: init env variables

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>TodoMVC Supabase</h1>
+    <p>{{ supabaseUrl}}</p>
   </div>
 </template>
 
+<script setup>
+const config = useRuntimeConfig()
+const {supabaseUrl} = config
+
+console.log(config)
+</script>
